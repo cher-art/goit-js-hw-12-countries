764377e791fae2cd69bad0f8175032b5f6f1bc15
TimChern111 <66830325+TimChern111@users.noreply.github.com>
2020-10-04 21:09:58 +0300

    Fin commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import css from './css/style.css';
+import { alert,error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import '../node_modules/@pnotify/core/dist/BrightTheme.css';
+import '../node_modules/@pnotify/core/dist/PNotify.css';
+import debounce from 'lodash.debounce';
+import template from './template/template.hbs';
+defaultModules.set(PNotifyMobile, {});
+const input = document.getElementById('get-country');
+const div = document.getElementById('country-box');
+let name, norice;
+
+input.addEventListener('input', debounce(() => {
+    name = input.value;
+    div.innerHTML = '';
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+    .then(response => {
+        if(response.status == 200 ){
+            return  response.json();
+        }else if(!name){
+        div.innerHTML = '';
+        }else{
+        norice = alert({title:'NOT Found', hide: true, delay: 1000})
+    }
+    })
+    .then(data => {
+        if(data.length > 10|| !data.length) {
+            return error({text: 'Введите точный запрос'})
+        }
+        const country = template(data)
+        div.insertAdjacentHTML('afterbegin', country);
+    })
+    .catch(err => {
+        div.innerHTML = '<h1>LOL: 404</h1>';
+        console.log(err);
+    })
+}, 500))
